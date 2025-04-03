55fd8527694b8faf5673e4a9b7d54c583e8a358e
Isra <israelibrahim20@gmail.com>
2025-04-03 11:01:21 +0300

    updated session_info function

--- main.py
+++ main.py
@@ -18,29 +18,32 @@ async def start_session():
 async def join_session(username, session_id):
     if session_id not in sessions:
         return {"error, session id not found"}
-    else:
-        if len(active_sessions) == 0:
-            active_sessions[session_id] = [
-                {
-                    "username": username,
-                    "choice": random.choice(["rock", "paper", "scissors"]),
-                }
-            ]
-            return {"data": active_sessions}
 
-        elif (
-            len(active_sessions[session_id]) < 2
-            and username not in active_sessions[session_id]
-        ):
-            active_sessions[session_id].append(
-                {
-                    "username": username,
-                    "choice": random.choice(["rock", "paper", "scissors"]),
-                }
-            )
-            return {"data": active_sessions}
-        else:
-            return {"message": "error, session full, or username already in use"}
+    print(active_sessions)
+    if session_id in active_sessions:
+        print(active_sessions[session_id])
+
+    if session_id not in active_sessions:
+        active_sessions[session_id] = [
+            {
+                "username": username,
+                "choice": random.choice(["rock", "paper", "scissors"]),
+            }
+        ]
+        return {"data": active_sessions}
+    elif (
+        len(active_sessions[session_id]) < 2
+        and username not in active_sessions[session_id]
+    ):
+        active_sessions[session_id].append(
+            {
+                "username": username,
+                "choice": random.choice(["rock", "paper", "scissors"]),
+            }
+        )
+        return {"data": active_sessions}
+    else:
+        return {"message": "error, session full, or username already in use"}
 
 
 @app.get("/api/session_info")
@@ -64,5 +67,7 @@ async def session_info(session_id):
                 return {"result": f"{user1_username} won!"}
             else:
                 return {"result": f"{user2_username} won!"}
+        else:
+            return {"error": "error, session not found"}
     except KeyError:
         return {"error": "error, session not found"}
--- test.py
+++ test.py
@@ -9,7 +9,8 @@ session_id = req.json()["session_id"]
 
 req2 = requests.get(url + f"/api/join_session?session_id={session_id}&username=jack")
 req3 = requests.get(url + f"/api/join_session?session_id={session_id}&username=jackson")
-req4 = requests.get(url + f"/api/session_info?session_id={session_id}")
+req4 = requests.get(url + f"/api/join_session?session_id={session_id}&username=json")
+req5 = requests.get(url + f"/api/session_info?session_id={session_id}")
+req6 = requests.get(url + f"/api/session_info?session_id={1232334}")
 
-
-print(req4.json())
+print(req2.json(), req3.json(), req4.json(), req5.json(), req6.json())
